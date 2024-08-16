24ceed36747fbbaa842078a0b5988a65057e00db
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-08-15 22:18:38 -0230

    Adding home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,9 @@
 import React, {useState} from 'react'
 import "./Navbar.css";
 import { Link, NavLink } from 'react-router-dom'
-import { createPortal } from "react-dom";
-import Modal from "./Modal";
 
 export const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [modalOpen, setModalOpen] = useState(false);
-    const handleButtonClick = (value) => {
-    setModalOpen(false);};
- 
+    const [menuOpen, setMenuOpen] = useState(false); 
     return (
     <nav>
         <Link to="/" className='logo'>Portfolio</Link>
@@ -25,58 +19,7 @@ export const Navbar = () => {
             <button className="primary-button" onClick={() => setModalOpen(true)}>
             Login
           </button>
-        </ul>
-
-        <div className="login-modal">
-        {modalOpen &&
-          createPortal(
-            <Modal
-              closeModal={handleButtonClick}
-              onSubmit={handleButtonClick}
-              onCancel={handleButtonClick}
-            >
-              <div className="login-header">Login</div>
-
-              <label for="email">
-                <b>Email</b>
-              </label>
-              <input
-                aria-label="email"
-                type="text"
-                placeholder="Enter Email"
-                name="email"
-                required
-              />
-
-              <label for="password">
-                <b>Password</b>
-              </label>
-              <input
-                aria-label="password"
-                type="text"
-                placeholder="Enter Password"
-                name="password"
-                required
-              />
-
-              <label>
-                <input
-                  aria-label="remember-me-checkbox"
-                  type="checkbox"
-                  checked="checked"
-                  name="remember"
-                  style={{ marginRight: "0.5rem" }}
-                />
-                Remember me
-              </label>
-              <span className="login-forgot-password">
-                Forgot <a href="#">password?</a>
-              </span>
-            </Modal>,
-            document.body
-          )}
-      </div>
-      
+        </ul>  
     </nav>
   );
 };
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="App">
+      <div className="home-container">
+        <div className="home-banner-container">
+          <div className="home-bannerImage-container"></div>
+          <div className="home-text-section">
+            <div className="home-heading">Lorem ipsum</div>
+            <div className="home-primary-text">
+              Lorem ipsum dolor sit amet consectetur. Ullamcorper elementum
+              aliquam fermentum orci. Tristique quis a sit eget. Quis donec
+              risus varius aenean pharetra cursus tellus magna ut.Quam ornare
+              quis in sit faucibus ut dolor.
+            </div>
+            <button className="secondary-button">Sign Up</button>
+          </div>
+          <div className="home-image-section">
+          </div>
+        </div>
+      </div>
+      </div>
   )
 }
 
