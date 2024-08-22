ba4a60d0926184322eab7c7a9da2788b1120bf61
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-08-22 20:01:51 -0230

    Adding modals

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,15 @@ import Home from './components/Pages/Home'
 import Artwork from './components/Pages/Artwork'
 import Account from './components/Pages/Account'
 import Contact from './components/Pages/Contact'
-import { Modal } from './components/Modal'
+
+
+
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Modal>
-        <h1>This is the modal header</h1>
-        <p>This is the modal description</p>
-      </Modal>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Artwork" element={<Artwork />} />
