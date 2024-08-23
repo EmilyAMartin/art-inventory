2f2ce75c656ffb803965ea541f20f49ca1844715
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-08-22 21:37:05 -0230

    Adding modal

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,26 @@ import Contact from './components/Pages/Contact'
 
 
 
+import Modal from './components/Modal'
+import { useState } from 'react'
 
 function App() {
+  const [modalOpen] = useState(false)
   return (
     <div className='App'>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Artwork" element={<Artwork />} />
         <Route path="/Account" element={<Account />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
+
+      {modalOpen && <Modal>
+        <h1>This is the modal header</h1>
+        <p>This is the modal description</p>
+      </Modal>}
+
     </div>
   )
 }
