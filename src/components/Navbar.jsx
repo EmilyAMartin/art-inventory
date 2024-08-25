5eb9b5335b27452230a4ed4de4b96e81b0f2caa2
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-08-24 22:17:16 -0230

    Added login in modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,15 @@ import "./Navbar.css";
 import { Link, NavLink } from 'react-router-dom'
 import { BsList } from "react-icons/bs";
 
+import Modal from './Modal';
+import { createPortal } from 'react-dom'
+
+
 export const Navbar = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleButtonClick = (value) => {
+    setModalOpen(false);
+  };
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav>
@@ -20,6 +28,49 @@ export const Navbar = () => {
           Login
         </button>
       </ul>
+      {modalOpen && (
+        createPortal(<Modal onSubmit={handleButtonClick} onCancel={handleButtonClick} onClose={handleButtonClick}>
+
+          <div className='login-form-container'>
+            <div className="login-header">Login</div>
+            <label for="email">
+              <b>Email</b>
+            </label>
+            <input
+              aria-label="email"
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              required
+            />
+
+            <label for="password">
+              <b>Password</b>
+            </label>
+            <input
+              aria-label="password"
+              type="text"
+              placeholder="Enter Password"
+              name="password"
+              required
+            />
+
+            <label>
+              <input
+                aria-label="remember-me-checkbox"
+                type="checkbox"
+                checked="checked"
+                name="remember"
+                style={{ marginRight: "0.5rem" }}
+              />
+              Remember me
+            </label>
+            <span className="login-forgot-password">
+              Forgot <a href="#">password?</a>
+            </span>
+          </div>
+        </Modal>, document.body)
+      )}
     </nav>
   );
 };
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -31,8 +31,56 @@ const Home = () => {
       </div>
       {modalOpen && (
         createPortal(<Modal onSubmit={handleButtonClick} onCancel={handleButtonClick} onClose={handleButtonClick}>
-          <h1>This is the modal header</h1>
-          <p>This is the modal description</p>
+          <div className='registration-container'>
+            <div className="registration-header">Sign Up</div>
+            <div className="registration-primary-text">{" "}
+              Please fill in this form to create an account.
+            </div>
+
+            <label for="email">
+              <b>Email</b>
+            </label>
+            <input
+              aria-label="email"
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              required
+            />
+
+            <label for="password">
+              <b>Password</b>
+            </label>
+            <input
+              aria-label="password"
+              type="text"
+              placeholder="Enter Password"
+              name="password"
+              required
+            />
+
+            <label for="repeat-password">
+              <b>Repeat Password</b>
+            </label>
+            <input
+              aria-label="repeat-password"
+              type="text"
+              placeholder="Repeat Password"
+              name="repeat-password"
+              required
+            />
+
+            <label>
+              <input
+                aria-label="remember-me-checkbox"
+                type="checkbox"
+                checked="checked"
+                name="remember"
+                style={{ marginRight: "0.5rem" }}
+              />
+              Remember me
+            </label>
+          </div>
         </Modal>, document.body)
       )}
     </div>
