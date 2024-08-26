381588f952f8dc52418e6160a1e4f7265993ea94
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-08-26 20:42:20 -0230

    fixing heart icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,11 @@ import "./Card.css"
 const Card = () => {
     return (
         <div className='art-card'>
-            <div className='box'> <BsFillHeartFill /></div>
+            <div className='box'>
+                <div className='favorite-icon'>
+                    <BsFillHeartFill />
+                </div>
+            </div>
             <h3 >Title</h3>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Amet vel quidem dicta nobis deserunt.</div>
