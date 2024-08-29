932e7b31bc7df70d879dec044a58c0ddb0c816bd
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-08-28 22:02:23 -0230

    Working on imported cards

---
+++ src/components/MediaCard.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+
+export default function MediaCard() {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                alt="painting"
+                height="140"
+                src="./Images/p1.jpg"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Untitled
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
+}
