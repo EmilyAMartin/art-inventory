ade1d7b85871c26a901bb01de862e77c4176e3c3
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-08-30 21:39:59 -0230

    Working on modals

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,42 @@ import Modal from './Modal';
 import { createPortal } from 'react-dom'
 
 
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Typography from '@mui/material/Typography';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+
+
+
+
 export const Navbar = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const handleButtonClick = (value) => {
     setModalOpen(false);
   };
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
+
     <nav>
       <Link to="/" className='logo'><img src="./Images/Logo.png" alt="logo portfolio" /></Link>
       <div className='menu' onClick={() => setMenuOpen(!menuOpen)}>
@@ -30,46 +59,46 @@ export const Navbar = () => {
       </ul>
       {modalOpen && (
         createPortal(<Modal onSubmit={handleButtonClick} onCancel={handleButtonClick} onClose={handleButtonClick}>
-          <div className='login-form-container'>
-            <div className="login-header">Login</div>
-            <label for="email">
-              <b>Email</b>
-            </label>
-            <input
-              aria-label="email"
-              type="text"
-              placeholder="Enter Email"
-              name="email"
-              required
-            />
-
-            <label for="password">
-              <b>Password</b>
-            </label>
-            <input
-              aria-label="password"
-              type="text"
-              placeholder="Enter Password"
-              name="password"
-              required
-            />
-
-            <label>
-              <input
-                aria-label="remember-me-checkbox"
-                type="checkbox"
-                checked="checked"
-                name="remember"
-                style={{ marginRight: "0.5rem" }}
+          <LockOpenIcon fontSize='large' />
+          <Typography gutterBottom variant="h5" component="div" paddingBottom={2}>Login</Typography>
+          <Typography gutterBottom variant="h7" component="div" paddingBottom={1}>Welcome, please login to continue</Typography>
+          <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div>
+              <TextField
+                label=" Email"
+                id="outlined-start-adornment"
+                sx={{ m: 1, width: '25ch' }}
+                slotProps={{
+                  input: {
+                    startAdornment: <InputAdornment position="start">Email</InputAdornment>,
+                  },
+                }}
               />
-              Remember me
-            </label>
-            <span className="login-forgot-password">
-              Forgot <a href="#">password?</a>
-            </span>
-          </div>
+              <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={showPassword ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        onMouseUp={handleMouseUpPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+            </div>
+          </Box>
         </Modal>, document.body)
       )}
-    </nav>
+    </nav >
   );
 };
