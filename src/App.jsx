2f26ee186c613e81eeac4163c0adabcd7ac50dee
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-09-05 21:57:08 -0230

    Working on testing api calls

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,39 @@ import Home from './components/Pages/Home'
 import Artwork from './components/Pages/Artwork'
 import Account from './components/Pages/Account'
 import Gallery from './components/Pages/Gallery'
-import ArtData from './components/ArtData.json'
+
+import axios from 'axios'
+import { useState } from 'react'
 
 function App() {
+  const [quote, setQuote] = useState('')
+  const getQuote = () => {
+    axios.get('https://api.quotable.io/random')
+      .then(res => {
+        console.log(res)
+        setQuote(res.data.content)
+      }).catch(err => {
+        console.log(err)
+      })
+  }
   return (
     <div className='App'>
       <Navbar />
+
+      <div>
+        <button onClick={getQuote}>Get Quote</button>
+        {quote && <div>{quote}</div>}
+      </div>
+
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Artwork" element={<Artwork />} />
         <Route path="/Gallery" element={<Gallery />} />
         <Route path="/Account" element={<Account />} />
       </Routes>
+
+
     </div>
   )
 }
--- src/components/Pages/Gallery.jsx
+++ src/components/Pages/Gallery.jsx
@@ -1,10 +1,61 @@
 import React, { useEffect } from "react";
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+
+
+import Grid2 from '@mui/material/Grid2';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import Container from '@mui/material/Container';
+import Data from '../ArtData.json'
+
+
+
 
 
 const Gallery = () => {
 
   return (
-    <div>
+    <div className="artwork-container">
+      <div>
+        <Container maxWidth="lg">
+          <Grid2 container spacing={5} style={{ marginTop: "10px" }}>
+            {Data.map((result, index) => (
+              <Grid2 item xs={12} ms={4} key={index}>
+                <Card sx={{ maxWidth: 250 }}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={result.image}
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Untitled
+                      </Typography>
+                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        Artist Name
+                      </Typography>
+                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        2023
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+
+              </Grid2>
+            ))}
+          </Grid2>
+        </Container>
+      </div>
+      <div>
+      </div>
+
     </div>
 
   )
