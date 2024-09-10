99414a2985ef33cfe4cae9ae710a72cfb37fc13a
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-09-09 22:41:01 -0230

    Working on displaying art API

--- src/components/Pages/Gallery.jsx
+++ src/components/Pages/Gallery.jsx
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
-
-
 import Grid2 from '@mui/material/Grid2';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -18,7 +13,7 @@ import Data from '../ArtData.json'
 
 
 const Gallery = () => {
-  const [posts, setPosts] = useState([]);
+  const [artwork, setArtwork] = useState([]);
 
   useEffect(() => {
     fetchData()
@@ -26,42 +21,28 @@ const Gallery = () => {
 
   const fetchData = async () => {
     const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts")
-    setPosts(data)
+    setArtwork(data)
   }
 
   return (
-    <div className="artwork-container">
-      <div>
-        {posts.map(post => (
-          <div key={post.id}>
-            <h5>{post.title}</h5>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
 
+    <div className="artwork-container">
       <div>
         <Container maxWidth="lg">
           <Grid2 container spacing={5} style={{ marginTop: "10px" }}>
-            {Data.map((result, index) => (
-              <Grid2 item xs={12} ms={4} key={index}>
+            {artwork.map(data => (
+              <Grid2 item xs={12} ms={4} key={data.id}>
                 <Card sx={{ maxWidth: 250 }}>
                   <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={result.image}
-                      alt=""
-                    />
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="div">
-                        {result.title}
+                        {data.title}
                       </Typography>
                       <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                        {result.artist}
+                        {data.body}
                       </Typography>
                       <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                        {result.date}
+                        2023
                       </Typography>
                     </CardContent>
                   </CardActionArea>
