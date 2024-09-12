84fe9453d5b21d3f8f9865b024479bd4bad8ed06
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-09-12 21:03:54 -0230

    Working on account page

--- src/components/Pages/Gallery.jsx
+++ src/components/Pages/Gallery.jsx
@@ -8,22 +8,54 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import Container from '@mui/material/Container';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
 
 const Gallery = () => {
   const [artwork, setArtwork] = useState([]);
-
   useEffect(() => {
     fetchData()
   }, [])
-
   const fetchData = async () => {
     const response = await axios.get("https://api.artic.edu/api/v1/artworks?page=2")
     setArtwork(response.data.data)
   }
 
+  const [search, setSearch] = useState([]);
+  useEffect(() => {
+    searchArtwork()
+  }, [])
+  const searchArtwork = async () => {
+    const response = await axios.get("https://api.artic.edu/api/v1/artworks")
+    setSearch(response.data.data)
+  }
+
   return (
     <div className="artwork-container">
       <div>
+
+        <Stack spacing={2} sx={{ width: 300 }} style={{ paddingLeft: 10 }}>
+          <Autocomplete
+            disableClearable
+            options={search.map((response) => response.title)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Search"
+                slotProps={{
+                  input: {
+                    ...params.InputProps,
+                    type: 'search',
+                  },
+                }}
+              />
+            )}
+          />
+        </Stack>
+
         <Container maxWidth="lg">
           <Grid2 container spacing={5} style={{ marginTop: "10px" }}>
             {artwork.map(art => (
@@ -36,11 +68,12 @@ const Gallery = () => {
                       image={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
                       alt=""
                     />
-
                     <CardContent>
                       <Typography gutterBottom variant="h6" component="div">{art.title}</Typography>
                       <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.artist_title}</Typography>
+                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.place_of_origin}</Typography>
                       <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.date_end}</Typography>
+                      <FavoriteBorderIcon style={{ float: 'right', margin: "10" }} />
                     </CardContent>
                   </CardActionArea>
                 </Card>
