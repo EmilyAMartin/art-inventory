071e9aaea93aafcbe3cc0f02c785dedd7c8f974d
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-09-14 22:26:33 -0230

    Account menu

--- src/components/Pages/Account.jsx
+++ src/components/Pages/Account.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useState } from 'react'
-import Drawer from '@mui/material/Drawer'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
+import { Drawer, Box, Typography, IconButton } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HelpIcon from '@mui/icons-material/Help';
 
 const Account = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
@@ -14,10 +14,10 @@ const Account = () => {
         <MenuIcon />
       </IconButton>
       <Drawer anchor='left' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
-        <Box p={2} width='250px' textAlign='center' role='presentation'>
-          <Typography variant='h6' component='div'>
-            Side Panel
-          </Typography>
+        <Box p={2} width='250px' textAlign='left' role='presentation'>
+          <Typography><AccountCircleIcon />Account</Typography>
+          <Typography><SettingsIcon />Settings</Typography>
+          <Typography><HelpIcon />Help</Typography>
         </Box>
       </Drawer>
     </>
