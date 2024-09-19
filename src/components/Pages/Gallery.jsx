b8cba9decaf5827a6d7297470c3aaae236456908
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-09-19 20:55:06 -0230

    Page navagation for gallery is working

--- src/components/Pages/Gallery.jsx
+++ src/components/Pages/Gallery.jsx
@@ -13,47 +13,69 @@ import Button from '@mui/material/Button';
 const Gallery = () => {
   const BASE_URL = "https://api.artic.edu/api/v1/artworks";
   const [artwork, setArtwork] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
 
   useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
     const fetchData = async () => {
-      const { data } = await axios.get(BASE_URL);
-      setArtwork(data.data)
+      try {
+        const { data } = await axios.get(`${BASE_URL}?page=${page}`, {
+          signal,
+        });
+        setArtwork(data.data);
+        setError(null);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Fetch aborted");
+          return;
+        }
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchData();
-  }, []);
+    return () => {
+      abortController.abort();
+      setIsLoading(true);
+    };
+  }, [page]);
 
   return (
-    <div className="artwork-container">
-      <div>
-
-        <Button disabled={page === 1} onClick={() => setPage((prevState) => prevState - 1)}> Prev </Button>
-        <Button onClick={() => setPage((prevState) => prevState + 1)}> Next </Button>
-        <Grid2 margin='auto' container spacing={5} style={{ marginTop: "10px" }}>
-          {artwork.map(art => (
-            <Grid2 item xs={12} ms={5} key={art.id}>
-              <Card sx={{ maxWidth: 200 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
-                    alt=""
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">{art.title}</Typography>
-                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.artist_title}</Typography>
-                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.place_of_origin}</Typography>
-                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.date_end}</Typography>
-                    <FavoriteBorderIcon style={{ float: 'right', margin: "10" }} />
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid2>
-          ))}
-        </Grid2>
-
+    <div id='galley-container' style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', margin: '2rem 7rem' }}>
+      <div id='page-navigation'>
+        <Button onClick={() => setPage(page - 1)}> Prev</Button>
+        <Button onClick={() => setPage(page + 1)}> Next</Button>
+        {error && <div>{error}</div>}
+        {isLoading && <div>Loading...</div>}
       </div>
+
+      <Grid2 margin='auto' container spacing={5} style={{ marginTop: "10px" }}>
+        {artwork.map(art => (
+          <Grid2 item xs={12} ms={5} key={art.id}>
+            <Card sx={{ maxWidth: 200 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
+                  alt=""
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="div">{art.title}</Typography>
+                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.artist_title}</Typography>
+                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.place_of_origin}</Typography>
+                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.date_end}</Typography>
+                  <FavoriteBorderIcon style={{ float: 'right', margin: "10" }} />
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid2>
+        ))}
+      </Grid2>
     </div>
   )
 }
