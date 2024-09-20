6d55d8b4217a648548ee5f5285079c24ebf885a0
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-09-20 21:23:49 -0230

    Adding search bar

--- src/components/Pages/Gallery.jsx
+++ src/components/Pages/Gallery.jsx
@@ -10,12 +10,47 @@ import CardActionArea from '@mui/material/CardActionArea';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import Button from '@mui/material/Button';
 
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+
+const SearchBar = ({ setSearchQuery }) => (
+  <form>
+    <TextField
+      id="search-bar"
+      className="text"
+      onInput={(e) => {
+        setSearchQuery(e.target.value);
+      }}
+      label="Search Keyword"
+      variant="outlined"
+      placeholder="Search..."
+      size="small"
+    />
+    <IconButton type="submit" aria-label="search">
+      <SearchIcon style={{ fill: "blue" }} />
+    </IconButton>
+  </form>
+);
+
+const filterData = (query, data) => {
+  if (!query) {
+    return data;
+  } else {
+    return data.filter((d) => d.toLowerCase().includes(query));
+  }
+};
+
+const data = []
+
 const Gallery = () => {
   const BASE_URL = "https://api.artic.edu/api/v1/artworks";
   const [artwork, setArtwork] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  const dataFiltered = filterData(searchQuery, data);
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -46,9 +81,42 @@ const Gallery = () => {
 
   return (
     <div id='galley-container' style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', margin: '2rem 7rem' }}>
+      <div
+        id='search-bar'
+        style={{
+          display: "flex",
+          alignSelf: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          padding: 20
+        }}
+      >
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <div style={{ padding: 3 }}>
+          {dataFiltered.map((d) => (
+            <div
+              className="text"
+              style={{
+                padding: 5,
+                justifyContent: "normal",
+                fontSize: 20,
+                color: "blue",
+                margin: 1,
+                width: "250px",
+                BorderColor: "green",
+                borderWidth: "10px"
+              }}
+              key={d.id}
+            >
+              {d}
+            </div>
+          ))}
+        </div>
+      </div>
+
       <div id='page-navigation'>
-        <Button onClick={() => setPage(page - 1)}> Prev</Button>
-        <Button onClick={() => setPage(page + 1)}> Next</Button>
+        <Button color='black' onClick={() => setPage(page - 1)}> Prev</Button>
+        <Button color='black' onClick={() => setPage(page + 1)}> Next</Button>
         {error && <div>{error}</div>}
         {isLoading && <div>Loading...</div>}
       </div>
