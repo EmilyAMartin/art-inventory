d21b5bf7beb98b0433a4932b6175134f84e4ef56
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-09-25 21:12:18 -0230

    Updated popover on gallery page

--- src/components/Pages/Gallery.jsx
+++ src/components/Pages/Gallery.jsx
@@ -26,11 +26,11 @@ const Gallery = () => {
   const [page, setPage] = useState(1);
   const [anchorEl, setAnchorEl] = useState(null);
   const [popoverImageId, setPopoverImageId] = useState(null);
-  const [searchQuery, setSearchQuery] = useState("");
+
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
-    setPopoverImageId[event.target.src]
+    setPopoverImageId(event.target.src)
   };
 
   const handleClose = () => {
@@ -41,45 +41,7 @@ const Gallery = () => {
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
 
-  const fetchDataByKeyword = async () => {
-    const response = await axios.get(`${BASE_URL}/search?q=${searchQuery}`);
-    console.log(response.data.data);
-    const data = response.data.data;
-    const fetchedData = await Promise.all(
-      data.map(async (art) => {
-        return await fetchDataById(art.id);
-      })
-    );
-    console.log(fetchedData);
-    setArtwork(fetchedData);
-  };
 
-  const fetchDataById = async (id) => {
-    const response = await axios.get(`${BASE_URL}/${id}`);
-    console.log(response.data.data);
-    return response.data.data;
-  };
-
-  const SearchBar = ({ setSearchQuery }) => (
-    <form>
-      <TextField
-        id="search-bar"
-        className="text"
-        onInput={(e) => {
-          setSearchQuery(e.target.value);
-        }}
-        label="Search Keyword"
-        variant="outlined"
-        placeholder="Search..."
-        size="small"
-      />
-      <IconButton type="submit" onClick={{ fetchDataByKeyword }}
-        aria-label="search">
-        <SearchIcon style={{ fill: "black" }}
-        />
-      </IconButton>
-    </form>
-  );
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -110,40 +72,6 @@ const Gallery = () => {
 
   return (
     <div id='galley-container' style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', margin: '2rem 7rem' }}>
-      <div
-        id='search-bar'
-        style={{
-          display: "flex",
-          alignSelf: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-          padding: 20
-        }}
-      >
-
-        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-        <div style={{ padding: 3 }}>
-          {dataFiltered.map((d) => (
-            <div
-              className="text"
-              style={{
-                padding: 5,
-                justifyContent: "normal",
-                fontSize: 20,
-                color: "blue",
-                margin: 1,
-                width: "250px",
-                BorderColor: "green",
-                borderWidth: "10px"
-              }}
-              key={d.id}
-            >
-              {d}
-            </div>
-          ))}
-        </div>
-      </div>
-
       <div id='page-navigation'>
         <Button color='black' onClick={() => setPage(page - 1)}> Prev</Button>
         <Button color='black' onClick={() => setPage(page + 1)}> Next</Button>
