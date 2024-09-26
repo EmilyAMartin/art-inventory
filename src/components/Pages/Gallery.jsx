39733e16810c214bddc4704e356923721aae0afe
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-09-25 22:26:32 -0230

    Working on search bar

--- src/components/Pages/Gallery.jsx
+++ src/components/Pages/Gallery.jsx
@@ -9,14 +9,11 @@ import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import Button from '@mui/material/Button';
+import Popover from '@mui/material/Popover';
 
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
-
-import Popover from '@mui/material/Popover';
-
-
 
 const Gallery = () => {
   const BASE_URL = "https://api.artic.edu/api/v1/artworks";
@@ -26,22 +23,56 @@ const Gallery = () => {
   const [page, setPage] = useState(1);
   const [anchorEl, setAnchorEl] = useState(null);
   const [popoverImageId, setPopoverImageId] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
-
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
     setPopoverImageId(event.target.src)
   };
-
   const handleClose = () => {
     setAnchorEl(null);
-    setPopoverImageId(null);
+    setPopoverImageId(null)
   };
 
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+  const fetchDataByKeyword = async () => {
+    const response = await axios.get(`${BASE_URL}/search?q=${searchQuery}`);
+    console.log(response.data.data);
+    const data = response.data.data;
+    const fetchedData = await Promise.all(
+      data.map(async (art) => {
+        return await fetchDataById(art.id);
+      })
+    );
+    console.log(fetchedData);
+    setArtwork(fetchedData);
+  };
 
+  const fetchDataById = async (id) => {
+    const response = await axios.get(`${BASE_URL}/${id}`);
+    console.log(response.data.data);
+    return response.data.data;
+  };
 
+  const SearchBar = (setSearchQuery) => (
+    <div>
+      <TextField
+        id="search-bar"
+        className="text"
+        onInput={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        label="Search Keyword"
+        variant="outlined"
+        placeholder="Search..."
+        size="small"
+      />
+      <IconButton type="submit" onClick={fetchDataByKeyword} aria-label="search">
+        <SearchIcon style={{ fill: "black" }} />
+      </IconButton>
+    </div>
+  );
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -72,6 +103,9 @@ const Gallery = () => {
 
   return (
     <div id='galley-container' style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', margin: '2rem 7rem' }}>
+
+      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+
       <div id='page-navigation'>
         <Button color='black' onClick={() => setPage(page - 1)}> Prev</Button>
         <Button color='black' onClick={() => setPage(page + 1)}> Next</Button>
@@ -82,11 +116,11 @@ const Gallery = () => {
       <Grid2 margin='auto' container spacing={5} style={{ marginTop: "10px" }}>
         {artwork.map(art => (
           <Grid2 item xs={12} ms={5} key={art.id}>
-            <Card sx={{ maxWidth: 200, maxHeight: 600, }}>
+            <Card sx={{ maxWidth: 200 }}>
               <CardActionArea>
                 <CardMedia
                   component="img"
-                  height="140"
+                  height="200"
                   image={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
                   alt=""
                   onClick={handleClick}
@@ -100,6 +134,7 @@ const Gallery = () => {
                     vertical: 'bottom',
                     horizontal: 'left',
                   }}
+
                 >
                   <CardMedia
                     component="img"
