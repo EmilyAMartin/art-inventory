eaad7ad2c4b77ea600155d51c2c7aba9d064019b
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-10-01 22:14:20 -0230

    Updated Account page layout

--- src/components/Pages/Account.jsx
+++ src/components/Pages/Account.jsx
@@ -1,159 +1,70 @@
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import TextField from '@mui/material/TextField';
 import { BsPersonCircle } from "react-icons/bs";
 
-import { NavLink } from 'react-router-dom'
-
-
-const drawerWidth = 240;
-
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme }) => ({
-    flexGrow: 1,
-    paddingLeft: theme.spacing(32),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    variants: [
-      {
-        props: ({ open }) => open,
-        style: {
-          transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-          }),
-          marginLeft: 0,
-        },
-      },
-    ],
-  }),
-);
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
-
 const Account = () => {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
   return (
-    <Box sx={{}}>
-      <IconButton
-        onClick={handleDrawerOpen}
-        sx={[open && { display: 'none' },]}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-            zIndex: -1,
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: 20, gap: '1.5rem' }}>
-          <Typography>Account</Typography>
-          <Typography>Settings</Typography>
-          <Typography>Help</Typography>
-        </div>
-      </Drawer>
-      <Main open={open}>
-        <DrawerHeader />
-        <div style={{ gap: 25, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <BsPersonCircle fontSize={150} />
-          <Typography variant="h6">Jane Doe</Typography>
-        </div>
-        <div className='profile-section' style={{ marginBottom: 50 }}>
-          <Typography variant="h6">Profile</Typography>
-          <TextField
-            id="outlined-required"
-            fullWidth
-            margin="normal"
-            label="Full Name"
-            defaultValue=""
-          />
-          <TextField
-            id="outlined-required"
-            fullWidth
-            margin="normal"
-            label="Username"
-            defaultValue=""
-          />
-          <TextField
-            id="outlined-required"
-            fullWidth
-            margin="normal"
-            label="Email Address"
-            defaultValue=""
-          />
-          <TextField
-            id="outlined-multiline-static"
-            fullWidth
-            label="Bio"
-            multiline
-            rows={4}
-            defaultValue=""
-          />
-        </div>
-        <div className='security-section' style={{ marginBottom: 50 }}>
-          <Typography variant="h6">Password and Security</Typography>
-          <TextField
-            id="outlined-required"
-            fullWidth
-            margin="normal"
-            label="Current Password"
-          />
-          <TextField
-            id="outlined-required"
-            fullWidth
-            margin="normal"
-            label="New Password"
-          />
-          <TextField
-            id="outlined-required"
-            fullWidth
-            margin="normal"
-            label="Re-type New Password"
-          />
-        </div>
-      </Main>
-    </Box>
+    <>
+      <div className='profile-header' style={{ marginTop: 50, gap: 25, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <BsPersonCircle fontSize={150} />
+        <Typography variant="h6">Jane Doe</Typography>
+      </div>
+      <div className='profile-section' style={{ marginBottom: 50 }}>
+        <Typography variant="h6">Profile</Typography>
+        <TextField
+          id="outlined-required"
+          fullWidth
+          margin="normal"
+          label="Full Name"
+          defaultValue=""
+        />
+        <TextField
+          id="outlined-required"
+          fullWidth
+          margin="normal"
+          label="Username"
+          defaultValue=""
+        />
+        <TextField
+          id="outlined-required"
+          fullWidth
+          margin="normal"
+          label="Email Address"
+          defaultValue=""
+        />
+        <TextField
+          id="outlined-multiline-static"
+          fullWidth
+          label="Bio"
+          multiline
+          rows={4}
+          defaultValue=""
+        />
+      </div>
+      <div className='security-section' style={{ marginBottom: 50 }}>
+        <Typography variant="h6">Password and Security</Typography>
+        <TextField
+          id="outlined-required"
+          fullWidth
+          margin="normal"
+          label="Current Password"
+        />
+        <TextField
+          id="outlined-required"
+          fullWidth
+          margin="normal"
+          label="New Password"
+        />
+        <TextField
+          id="outlined-required"
+          fullWidth
+          margin="normal"
+          label="Re-type New Password"
+        />
+      </div>
+    </>
   )
 }
 export default Account
