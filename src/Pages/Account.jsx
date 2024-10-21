7f6a2b6933b72aed39a239ac75842b2b201507f8
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-10-21 20:25:24 -0230

    Working on form

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import { BsPersonCircle } from "react-icons/bs";
 
-import { useRef } from 'react';
 
 const Account = () => {
+
   const hiddenFileInput = useRef(null);
   const handleClick = event => {
     hiddenFileInput.current.click();
@@ -14,10 +14,25 @@ const Account = () => {
     const fileUploaded = event.target.files[0];
     handleFile(fileUploaded);
   };
+  const [formValues, updateFormValues] = useState([])
+  const [fname, setFirstName] = useState('')
+  const [lname, setLastName] = useState('')
+
+  useEffect(() => {
+    const formData = window.localStorage.getItem("form");
+    const savedValues = JSON.parse(formData);
+    updateFormValues(savedValues.formValues);
+    setFirstName(savedValues.fname)
+    setLastName(savedValues.lname)
+  }, []);
+
+  useEffect(() => {
+    const valuesToSave = { formValues, fname, lname }
+    window.localStorage.setItem("form", JSON.stringify(valuesToSave))
+  });
 
   return (
     <>
-
       <div className='profile-header' style={{ marginTop: 50, gap: 25, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
         <BsPersonCircle fontSize={150} className="button-upload" onClick={handleClick}>
         </BsPersonCircle>
@@ -37,6 +52,7 @@ const Account = () => {
           margin="normal"
           label="Full Name"
           defaultValue=""
+          onChange={e => setFirstName(e.target.value)}
         />
         <TextField
           id="outlined-required"
@@ -44,6 +60,7 @@ const Account = () => {
           margin="normal"
           label="Username"
           defaultValue=""
+          onChange={e => setLastName(e.target.value)}
         />
         <TextField
           id="outlined-required"
