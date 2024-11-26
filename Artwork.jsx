b7f193dabf31f97084b6c65d4b3809bb4a9da10a
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-11-25 21:20:12 -0330

    Updates

---
+++ Artwork.jsx
@@ -0,0 +1,155 @@
+import React, { useState, useEffect } from "react";
+import Grid2 from '@mui/material/Grid2';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import Data from '../components/ArtData.json'
+import Popover from '@mui/material/Popover';
+import AddArtworkBtn from "../components/AddArtworkBtn";
+import { Favorite } from '@mui/icons-material';
+import { FavoriteBorder } from '@mui/icons-material';
+
+const Artwork = () => {
+  const [artwork, setArtwork] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [popoverImageId, setPopoverImageId] = useState(null);
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  const handlePopClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    setPopoverImageId(event.target.src)
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+    setPopoverImageId(null)
+  };
+
+  const handleFilterChange = (e) => {
+    if (e.target.value === handleFilterChange(e.target.value)) {
+      setArtwork(Data);
+    } else if (e.target.value === handleFilterChange(e.target.value)) {
+      setArtwork(JSON.parse(localStorage.getItem('favoritesList')));
+    }
+    console.log(e.target.value)
+  }
+
+
+  const handleFavClick = (id) => {
+    const updateArtwork = artwork.map((item) => {
+      return item.id === id ? { ...item, favorite: !item.favorite } : item;
+    })
+    setArtwork(updateArtwork);
+    const selectedArtwork = updateArtwork.find((art) => art.id === id);
+    if (selectedArtwork.favorite === true) {
+      const favoritesList =
+        JSON.parse(localStorage.getItem("favoritesList")) ?? [];
+      favoritesList.push(selectedArtwork);
+      localStorage.setItem("favoritesList", JSON.stringify(favoritesList));
+    } else if (selectedArtwork.favorite === false) {
+      const favoritesList =
+        JSON.parse(localStorage.getItem("favoritesList")) ?? [];
+      const updatedFavoritesList = favoritesList.filter(
+        (art) => art.id !== selectedArtwork.id
+      );
+      localStorage.setItem(
+        "favoritesList",
+        JSON.stringify(updatedFavoritesList)
+      );
+    }
+  };
+
+  useEffect(() => {
+    setArtwork(Data)
+  }, [])
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: 15, marginTop: 25, }}>
+      <AddArtworkBtn />
+      < div className="select-filter"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignContent: 'center'
+        }}>
+        <select
+          style={{
+            borderColor: 'lightgrey',
+            borderRadius: '0.3rem',
+            width: '25rem',
+            height: '3rem',
+            fontSize: '1rem',
+          }}
+          onChange={(e) => handleFilterChange(e)}>
+          <option value="recent">Recently Added</option>
+          <option value="favorites">Favorites</option>
+        </select>
+
+        <div>
+          <Grid2 margin='auto' container spacing={8} style={{ marginTop: "25px", justifyContent: 'space-around' }}>
+            {artwork.map(art => (
+              <Grid2 item xs={12} ms={5} key={art.id}>
+                <Card sx={{ maxWidth: 300, maxHeight: 600, display: "flex" }}>
+                  <CardActionArea>
+                    <CardMedia
+                      style={{ width: 300, height: 300 }}
+                      component="img"
+                      image={art.image_path ? art.image_path : `https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
+                      onClick={handlePopClick}
+                    />
+                    <Popover
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                      }}
+                      id={id}
+                      open={open}
+                      anchorEl={anchorEl}
+                      anchorReference="none"
+                      onClose={handleClose}
+                      anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                      }}
+                    >
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={popoverImageId}
+                        alt=""
+                      />
+                    </Popover>
+                    <CardContent style={{ width: 300, height: 200 }}>
+                      <Typography gutterBottom fontSize={16} fontWeight={500} component="div">{art.title}</Typography>
+                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.artist_title}</Typography>
+                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.place_of_origin}</Typography>
+                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>{art.date_end}</Typography>
+                    </CardContent>
+                    {art.favorite === true && (
+                      <Favorite
+                        style={{ margin: 10 }}
+                        onClick={() => { handleFavClick(art.id) }}
+                      />
+                    )}
+                    {art.favorite === false && (
+                      <FavoriteBorder
+                        style={{ margin: 10 }}
+                        onClick={() => { handleFavClick(art.id) }}
+                      />
+                    )}
+                  </CardActionArea>
+                </Card>
+              </Grid2>
+            ))}
+          </Grid2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Artwork
