f22c1f025402ef52d4d1461b3687040f10419b97
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-11-25 22:16:32 -0330

    Edit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	siteUrl: 'https://emilyamartin.github.io/art-inventory/',
+	pathPrefix: '/<art-inventory>',
 });
