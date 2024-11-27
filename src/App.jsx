6464490e4d9d86bb2d3ed3252b0a727f0bc4a2fa
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-11-27 12:23:29 -0330

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import { Navbar } from "./components/Navbar";
-import Home from "./Pages/Home";
-import Artwork from "./Pages/Artwork";
-import Account from "./Pages/Account";
-import Gallery from "./Pages/Gallery";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Navbar } from './components/Navbar';
+import Home from './Pages/Home';
+import Artwork from './Pages/Artwork';
+import Account from './Pages/Account';
+import Gallery from './Pages/Gallery';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Artwork" element={<Artwork />} />
-        <Route path="/Gallery" element={<Gallery />} />
-        <Route path="/Account" element={<Account />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar />
+			<Routes>
+				<Route
+					path='/'
+					element={<Home />}
+				/>
+				<Route
+					path='/Artwork'
+					element={<Artwork />}
+				/>
+				<Route
+					path='/Gallery'
+					element={<Gallery />}
+				/>
+				<Route
+					path='/Account'
+					element={<Account />}
+				/>
+			</Routes>
+		</div>
+	);
 }
 export default App;
