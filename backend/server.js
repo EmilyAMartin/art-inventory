03df87c5c01d0cbd4691fdaa63772fd5419e17aa
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2025-01-23 22:16:39 -0330

    Working on art/artist tables

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
+// Default route
 app.get('/', (req, res) => {
-	res.send('Hello!');
+	res.send('Hello World!');
 });
 
+// Start the server
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
