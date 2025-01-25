5c93376ef1fb20d3527e8cd7cf2051ae38d68fe3
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2025-01-24 22:26:47 -0330

    Working on table

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,21 @@ const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: '',
+});
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+	if (err) throw err;
+
+	console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
 // Default route
 app.get('/', (req, res) => {
 	res.send('Hello World!');
