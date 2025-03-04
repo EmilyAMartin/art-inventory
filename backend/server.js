294c7b579f7eb6e30700cb0ea2cd53d159600c90
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2025-03-03 23:23:03 -0330

    Fixing missing user data. Working on error with profile

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,6 @@ app.use(
 		store: sessionStore,
 		resave: false,
 		saveUninitialized: false,
-		cookie: {
-			httpOnly: true, // Ensures cookies are not accessible via JS
-			secure: process.env.NODE_ENV === 'production', // Only set cookies over HTTPS in production
-			maxAge: 24 * 60 * 60 * 1000, // 1 day
-		},
 	})
 );
 
@@ -98,8 +93,8 @@ app.post('/register', async (req, res) => {
 	}
 });
 
+// Profile Route
 app.get('/profile', (req, res) => {
-	console.log('Session Data:', req.session.user); // Add this line to check session data
 	if (req.session.user) {
 		res.json({ user: req.session.user });
 	} else {
